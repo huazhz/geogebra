379d53df245259a56225f2411b3cc298d8a773a4
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-02-26 11:02:58 +0100

    remove duplicate after merge

--- common/src/main/java/org/geogebra/common/kernel/geos/GeoElement.java
+++ common/src/main/java/org/geogebra/common/kernel/geos/GeoElement.java
@@ -7748,14 +7748,6 @@ public abstract class GeoElement extends ConstructionElement
 		return this instanceof EquationValue;
 	}
 
-	public void setParentGroup(Group parentGroup) {
-		this.parentGroup = parentGroup;
-	}
-
-	public Group getParentGroup() {
-		return parentGroup;
-	}
-
     public void setParentGroup(Group parentGroup) {
         this.parentGroup = parentGroup;
     }
